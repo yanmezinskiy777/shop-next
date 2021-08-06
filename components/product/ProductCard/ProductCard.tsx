2dbd4b7a90e7790c8a76e260e91f664a9b7b4489
Yan Mezinskiy <yan.mezinskiy@setinbox.com>
2021-08-06 13:07:21 +0300

    cardproduct css

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -4,6 +4,7 @@ import { Product } from '@common/types/products'
 import React, { FC } from 'react'
 import Image from "next/image"
 import Link from "next/link"
+import style from "./ProductCard.module.css"
 
 interface Props{
     product: Product
@@ -14,14 +15,18 @@ const placeholder = "/placeholder.svg"
 const ProductCard: FC<Props> = ({ product }) => {
     return (
         <Link href={`/products/${product.slug}`}>
-            <a>
-                <h3>
-                    <span>
-                        {product.name}
-                    </span>
-                </h3>
-                <span>20 $</span>
+            <a className={style.root}>
+                <div className={style.productBg}></div>
+                <div className={style.productTag}>
+                    <h3 className={style.productTitle}>
+                        <span>
+                            {product.name}
+                        </span>
+                    </h3>
+                    <span className={style.productPrice}>20 $</span>
+                </div>
                 {product.images && <Image 
+                                    className={style.productImage}
                                     alt={product.name ?? "Product image"} 
                                     src={product.images[0].image ?? placeholder} 
                                     quality="90" 
